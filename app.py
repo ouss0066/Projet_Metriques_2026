1073103862dddcc200fcf11acf3b93b2828e467f
ouss0066 <ouss0066@gmail.com>
2026-05-18 15:35:27 +0200

    app.py

--- app.py
+++ app.py
@@ -34,6 +34,12 @@ def api_paris():
 @app.route("/rapport")
 def mongraphique():
     return render_template("graphique.html")
+
+@app.route("/histogramme")
+def histogramme():
+    return render_template("histogramme.html")
+
+
 # Ne rien mettre après ce commentaire
     
 if __name__ == "__main__":
